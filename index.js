3be37b28d18a8240d80d6f365bf0ad3aba120082
KRD13 <rauchdavis13@gmail.com>
2022-01-05 07:03:55 -0500

    tests working, now setting up index.js file

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const inquirer = require('inquirer');
 const Employee = require('../lib/Employee.js');
+const Engineer = require('../lib/Engineer.js');
+const Intern = require('../lib/Intern.js');
+const Mananger = require('../lib/Manager.js');
 
-class Generator {
-  constructor()
-}
+function addEmployee() {
+
+};
+
