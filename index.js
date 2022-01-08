1ad776f7f5877de7e8a485d1c7102016767c13ee
KRD13 <rauchdavis13@gmail.com>
2022-01-08 18:59:53 -0500

    question section corrected, doulbe use of .then for addEmployee, regorganized question flow

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const Employee = require('./lib/Employee.js');
+// const Employee = require('./lib/Employee.js');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
 const Manager = require('./lib/Manager.js');
@@ -13,8 +13,15 @@ const writeFile = require('./src/generate-site');
 // generates answers for Employee
 const addEmployee = () => {
 
+  //const employee = new Employee;
   // uses inquire to prompt user for a series of questions, and the answers will be returned
   return inquirer.prompt([
+    {
+      type: 'list',
+      name: 'role',
+      message:'Which role will this team member have?',
+      choices: ["Manager", "Engineer", "Intern"],
+      },
 
     {
       type: 'input',
@@ -56,21 +63,21 @@ const addEmployee = () => {
           return false;
         }
       }
-    },
-    {
-      type: 'list',
-      name: 'role',
-      message:'Which role will this team member have?',
-      choices: ["Manager", "Engineer", "Intern"],
-      
-      }
+    }   
   ])
   .then(answers => {
-    const employee = new Employee(answers.name, answers.id, answers.email, answers.role)
-    employeeArray.push(employee);
-    console.log(employee);
+    const employee = {
+      name: answers.name,
+      id: answers.id,
+      email: answers.email,
+      role: answers.role
+    }
+
+    // employeeArray.push(employee);
+    // console.log(employee);
     // addTeamMember();
     if (answers.role === "Manager") {
+
       addManager(employee);
     }
     else if (answers.role === "Engineer") {
@@ -78,12 +85,13 @@ const addEmployee = () => {
       }
     
     else if (answers.role === "Intern") {
+
       addIntern(employee);
     }
   })
 };    
 
-const addIntern = () => {
+const addIntern = (employee) => {
 
   // returns 'school' for intern
   return inquirer.prompt([
@@ -91,27 +99,32 @@ const addIntern = () => {
     { 
         type: 'input',
         name: 'school',
-        message: 'What school did you go to?',
+        message: 'What school did the intern go to?',
         validate: school => {
           if (school) {
             return true;
           } else {
-            console.log('Please enter the school you went to!');
+            console.log('Please enter the school the intern went to!');
             return false;
           }
         }
     },
   ])
   .then(answers => {
-    const intern = new Intern(answers.name, answers.id, answers.email, answers.role, answers.school)
+    const intern = new Intern(employee.name, employee.id, employee.email, employee.role, answers.school)
     employeeArray.push(intern);
+    // console.log(intern);
+    console.log(employeeArray);
     // addTeamMember();
     })
 };
 
-const addManager = () => {
+const addManager = (employee) => {
+  console.log(employee);
 
-  // returns 'cell phone' for manager
+  const manager = new Manager;
+ 
+
   return inquirer.prompt([
     { 
         type: 'input',
@@ -128,13 +141,17 @@ const addManager = () => {
     },
   ])
   .then(answers => {
-    const manager = new Manager(answers.name, answers.id, answers.email, answers.role, answers.officeNum)
-    employeeArray.push(Manager);
+    
+    const manager = new Manager(employee.name, employee.id, employee.email, employee.role, answers.officeNum)
+    
+    employeeArray.push(manager);
+    console.log(employeeArray);
     // addTeamMember();
     })
 };
 
-const addEngineer = () => {
+const addEngineer = (employee) => {
+  console.log(employee);
 
   // returns 'cell phone' for manager
   return inquirer.prompt([
@@ -153,8 +170,9 @@ const addEngineer = () => {
     },
   ])
   .then(answers => {
-    const engineer = new Engineer(answers.name, answers.id, answers.email, answers.roleanswers.gitHub)
-    employeeArray.push(Manager);
+    const engineer = new Engineer(employee.name, employee.id, employee.email, employee.role, answers.gitHub)
+    employeeArray.push(engineer);
+    console.log(employeeArray);
     // addTeamMember();
     })
 };
@@ -167,15 +185,16 @@ function init() {
   console.log(message);
   message2 = "Please answer the following quesitons for your Team Profile Generator file to be made.";
   console.log(message2);
+  addEmployee();
 };
 
 init();
 
 // old code, but use to connect to html builder
 // addEmployee data passed to page_template.js
-addEmployee()
-.then(answerData => {
-  console.log('The following data has been entered for your Team Profile');
-  console.log(answerData);
-  writeFile(page_template(answerData));
-})
+// addEmployee()
+// .then(answerData => {
+//   // console.log('The following data has been entered for your Team Profile');
+//   console.log(answerData);
+//   // writeFile(page_template(answerData));
+// })
